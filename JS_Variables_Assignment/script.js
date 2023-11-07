57b707684023fdbdbba95807d90368d045be72c2
Esharper <147178117+Esharper@users.noreply.github.com>
2023-11-06 23:45:07 -0600

    Add files via upload

---
+++ JS_Variables_Assignment/script.js
@@ -0,0 +1,44 @@
+"use strict";
+let serverName="Elgin Sharper";
+alert(serverName);
+let firstName=prompt ("what is your first name?");
+let lastName=prompt ("what is your last name?");
+let age=prompt("what is your age?");
+let isFavoriteFood= true;
+console.log(isFavoriteFood);
+let favoriteFood;
+console.log(favoriteFood);
+
+let fullName= firstName +  ' ' + lastName+ ' '+ age;
+console.log(fullName);
+const greeting=fullName;
+let price=99.95;
+console.log(price);
+let isMud=true;
+console.log(isMud);
+let isDirt=false;
+console.log(isDirt);
+let newHouse=undefined;
+console.log(newHouse);
+let emptyHouse=null;
+console.log(emptyHouse);
+let myString= "Hello i am Elgin!"
+console.log(myString);
+
+let num1="5";
+let num2="15";
+console.log(typeof num1, typeof num2);
+let reslt=num1+num2;
+
+let sentence= '\tI was up late doing but, "Java\nScript is awesome!"';
+
+console.log(sentence);
+
+let mathstring='one plus one equals ${1+1}';
+
+console.log(mathstring);
+console.log('string length:', greeting.length);
+console.log('indexing', greeting [1]);
+console.log(fullName);
+
+document.write(greeting);
